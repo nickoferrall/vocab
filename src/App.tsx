d8261e2c1a73ff67c52acb204bb7fb3a7251a4e8
nickoferrall <nickoferrall@gmail.com>
2020-12-06 11:51:12 +0000

    set-up styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,111 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useSpring, animated as a} from 'react-spring'
-import './styles.css'
+import styled from '@emotion/styled'
+
+const deck = [
+  {textOne: 'una gota', textTwo: 'a drop'},
+  {textOne: 'un chorro', textTwo: 'a stream'},
+  {textOne: 'un percance', textTwo: 'a mishap'},
+  {textOne: 'destacar', textTwo: 'to emphasize, to stand out'},
+  {textOne: 'contraer', textTwo: 'to contract'},
+  {textOne: 'reanudar', textTwo: 'to resume'},
+  {textOne: 'el afiche', textTwo: 'the poster'},
+  {textOne: 'el chubasco', textTwo: 'the heavy shower (weather)'},
+  {textOne: 'el telón', textTwo: 'the curtain'},
+  {textOne: 'atrapar', textTwo: 'to catch, capture'},
+  {textOne: 'manchar', textTwo: 'to stain'}
+]
+
+const Wrapper = styled('div')({
+  height: '100vh',
+  justifyContent: 'center',
+  alignItems: 'center',
+  display: 'flex',
+  width: '100%',
+  position: 'relative'
+})
+
+const Card = styled('div')<{isVisible: boolean}>(({isVisible}) => ({
+  display: isVisible ? 'flex' : 'none',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: 480,
+  width: 600,
+  background: '#0E2539',
+  overflow: 'scroll',
+  '&:hover': {
+    cursor: 'pointer'
+  }
+}))
+
+const Title = styled('h1')({
+  textAlign: 'center',
+  fontSize: 32,
+  color: '#FFFF',
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  maxWidth: 240
+})
+
+const initialCard = deck[Math.floor(Math.random() * deck.length)]
 
 function App() {
-  const [flipped, set] = useState(false)
+  const [isFlipped, setIsFlipped] = useState(false)
+  const [card, setCard] = useState(initialCard)
   const {transform, opacity} = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-    config: {mass: 5, tension: 500, friction: 80},
+    opacity: isFlipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${isFlipped ? 180 : 0}deg)`,
+    config: {mass: 5, tension: 500, friction: 80}
   })
+  const fade = useSpring({
+    from: {
+      color: 'red'
+    },
+    to: {
+      color: 'green'
+    }
+  })
+
+  useEffect(() => {
+    if (!isFlipped) {
+      const newCard = deck[Math.floor(Math.random() * deck.length)]
+      setCard(newCard)
+    }
+  }, [isFlipped])
+
+  const toggleisFlipped = () => {
+    setIsFlipped(!isFlipped)
+  }
+
   return (
-    <div onClick={() => set((state) => !state)}>
+    <Wrapper>
       <a.div
-        className="c back"
+        className="c"
         style={{
           opacity: opacity.interpolate((o: any) => (1 - o) as any),
-          transform,
+          transform
         }}
-      />
+      >
+        <Card onClick={toggleisFlipped} isVisible={!isFlipped}>
+          <a.div style={fade}>
+            <Title>{card.textOne}</Title>
+          </a.div>
+        </Card>
+      </a.div>
       <a.div
-        className="c front"
+        className="c"
         style={{
           opacity,
-          transform: transform.interpolate((t) => `${t} rotateX(180deg)`),
+          transform: transform.interpolate((t) => `${t} rotateX(180deg)`)
         }}
-      />
-    </div>
+      >
+        <Card onClick={toggleisFlipped} isVisible={isFlipped}>
+          <a.div style={fade}>
+            <Title>{card.textTwo}</Title>
+          </a.div>
+        </Card>
+      </a.div>
+    </Wrapper>
   )
 }
 
